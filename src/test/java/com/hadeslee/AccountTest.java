2a6a90ed1d07285711ccf390abe81ec489149cc7
hadesyi <hadesleeq@gmail.com>
2016-08-16 15:40:28 +0900

    account 테스트 케이스

--- src/test/java/com/hadeslee/AccountTest.java
+++ src/test/java/com/hadeslee/AccountTest.java
@@ -10,9 +10,21 @@ package com.hadeslee;
  * To change this template use File | Settings | File Templates.
  */
 public class AccountTest {
-    public void testAccount() {
+    public void testAccount() throws Exception {
         Account account = new Account();
+        if (account == null) {
+            throw new Exception("계좌생성 실패");
+        }
+    }
 
-
+    public static void main(String[] args) {
+        AccountTest test = new AccountTest();
+        try {
+            test.testAccount();
+        } catch (Exception e) {
+            System.out.println("실패(X)");
+            return;
+        }
+        System.out.println("성공(O)");
     }
 }
