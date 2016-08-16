ca686829a8ae2f4539a2a2cd31fe97ab49afb944
hadesyi <hadesleeq@gmail.com>
2016-08-16 15:25:06 +0900

    account 생성

---
+++ src/main/java/com/hadeslee/Account.java
@@ -0,0 +1,13 @@
+package com.hadeslee;
+
+/**
+ * Project: MyTddProject
+ * FileName: Account
+ * Date: 2016-08-16
+ * Time: 오후 3:23
+ * Author: Hadeslee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class Account {
+}
---
+++ src/test/java/com/hadeslee/AccountTest.java
@@ -0,0 +1,18 @@
+package com.hadeslee;
+
+/**
+ * Project: MyTddProject
+ * FileName: AccountTest
+ * Date: 2016-08-16
+ * Time: 오후 3:20
+ * Author: Hadeslee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class AccountTest {
+    public void testAccount() {
+        Account account = new Account();
+
+
+    }
+}
