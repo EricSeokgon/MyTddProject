55662e5ef29263ad3df251b53cdb7cffdc01a559
hadesyi <hadesleeq@gmail.com>
2016-08-16 16:20:27 +0900

    리펙토링

--- src/main/java/com/hadeslee/Account.java
+++ src/main/java/com/hadeslee/Account.java
@@ -20,11 +20,11 @@ public class Account {
         return this.balance;
     }
 
-    public void deposit(int i) {
-        this.balance += i;
+    public void deposit(int money) {
+        this.balance += money;
     }
 
-    public void withdraw(int i) {
-        this.balance -= i;
+    public void withdraw(int money) {
+        this.balance -= money;
     }
 }
