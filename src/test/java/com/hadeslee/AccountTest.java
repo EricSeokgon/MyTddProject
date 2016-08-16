91f749bc3c3d3727f26c7d132b0c06ef10ea003f
hadesyi <hadesleeq@gmail.com>
2016-08-16 15:45:38 +0900

    todo list 추가

--- src/test/java/com/hadeslee/AccountTest.java
+++ src/test/java/com/hadeslee/AccountTest.java
@@ -10,6 +10,15 @@ package com.hadeslee;
  * To change this template use File | Settings | File Templates.
  */
 public class AccountTest {
+    /* // TODO: 2016-08-16
+    * 1.클래스 이름은 Account --완료
+     * 2.기능은 세가지
+     * -잔고 조회
+     * -입금
+     * -출금
+     * 근액은 원 단위로(예: 천 원 = 1000)
+    * */
+
     public void testAccount() throws Exception {
         Account account = new Account();
         if (account == null) {
