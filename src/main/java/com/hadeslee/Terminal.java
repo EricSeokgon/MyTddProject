135526d97e772da6bfec2adf3e46f9cb13adb7f4
hadeslee <hadesleeq@gmail.com>
2016-11-11 03:22:17 +0900

    로그인 클래스

---
+++ src/main/java/com/hadeslee/Terminal.java
@@ -0,0 +1,29 @@
+package com.hadeslee;
+
+/**
+ * Created by hadeslee on 2016-11-11.
+ */
+public class Terminal {
+    private static Terminal term;
+
+    public void netConnect() {
+        return;
+    }
+
+    public void netDisconnect() {
+        return;
+    }
+
+    public String logon(String id, String pw) {
+
+        return id + ":" + pw;
+    }
+
+    public void logoff() {
+        return;
+    }
+
+    public void isLogon() {
+        return;
+    }
+}
