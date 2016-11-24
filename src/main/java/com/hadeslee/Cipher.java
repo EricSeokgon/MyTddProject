1e69a01126b1a0e5cf6f00c27f819bd49c2902c1
hadeslee <hadesleeq@gmail.com>
2016-11-25 01:50:06 +0900

    암호화 모듈 인터페이스

---
+++ src/main/java/com/hadeslee/Cipher.java
@@ -0,0 +1,9 @@
+package com.hadeslee;
+
+/**
+ * Created by hadeslee on 2016-11-25.
+ */
+public interface Cipher {
+    public String encrypt(String source);
+    public String decrypt(String source);
+}
