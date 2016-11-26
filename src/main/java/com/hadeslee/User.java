faca4519d453f98c51d9099a3e89fad8cbc8cd6c
hadeslee <hadesleeq@gmail.com>
2016-11-26 23:59:19 +0900

    유저 클래스

---
+++ src/main/java/com/hadeslee/User.java
@@ -0,0 +1,25 @@
+package com.hadeslee;
+
+/**
+ * Created by hadeslee on 2016-11-26.
+ */
+public class User {
+    private String userId;
+    private String Password;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
+}
