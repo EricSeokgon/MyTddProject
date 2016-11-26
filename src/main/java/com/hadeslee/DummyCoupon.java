800c669a6872512d320de4ef6aba476062b6e5fc
hadeslee <hadesleeq@gmail.com>
2016-11-26 23:49:10 +0900

    쿠폰 더미 객체

---
+++ src/main/java/com/hadeslee/DummyCoupon.java
@@ -0,0 +1,32 @@
+package com.hadeslee;
+
+/**
+ * Created by hadeslee on 2016-11-26.
+ */
+public class DummyCoupon implements ICupon {
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public boolean isValid() {
+        return false;
+    }
+
+    @Override
+    public int getDiscountPercent() {
+        return 0;
+    }
+
+    @Override
+    public boolean isAppliable(Item item) {
+        return false;
+    }
+
+    @Override
+    public void doExpire() {
+
+    }
+}
