f85f71f5c0bfdc73fdfabb3e37950bcf44900932
hadeslee <hadesleeq@gmail.com>
2016-11-29 01:29:29 +0900

    배열테스트

--- src/test/java/com/hadeslee/TestArrayEqual.java
+++ src/test/java/com/hadeslee/TestArrayEqual.java
@@ -2,6 +2,8 @@ package com.hadeslee;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+
 import static org.junit.Assert.assertArrayEquals;
 
 /**
@@ -13,5 +15,14 @@ public class TestArrayEqual {
         String[] arrayA = new String[]{"A", "B", "C"};
         String[] arrayB = new String[]{"A", "B", "C"};
         assertArrayEquals("두 배열의 값과 순서가 같아야 함", arrayA, arrayB);
+    }
+
+    @Test
+    public void testArryEqual_notSorted() {
+        String[] arrayA = new String[]{"A", "B", "C"};
+        String[] arrayB = new String[]{"A", "B", "C"};
+        Arrays.sort(arrayA);
+        Arrays.sort(arrayB);
+        assertArrayEquals("두 배열의 순서는 달라도 무방", arrayA, arrayB);
     }
 }
